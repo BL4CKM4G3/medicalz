e9a51cdcd94d70fe294cc0256449681d0608ae71
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-06 23:13:32 -0300

    feat: :sparkles: Created Sidebar
    Created Sidebar for layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      mini-variant
+      mini-variant-width="100"
+      class="cyan darken-1"
+    >
+      <v-list>
+        <v-list-item class="mb-12">
+          <v-list-item-action>
+
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-list flat>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-icon class="mb-2">{{ icons.dashboard }}</v-icon>
+            <v-list-item-subtitle align="center">Dashboard</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+import * as Icons from "../constants/icons";
+
+export default {
+  name: "Sidebar",
+  props: {
+    drawer: Boolean
+  },
+  data: () => ({
+    icons: {
+      dashboard: Icons.ICON_DASHBOARD
+    }
+  }),
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
